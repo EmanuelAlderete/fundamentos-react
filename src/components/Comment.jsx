f3c13843cb32f9cafa1c2b5e6f37d25c326f0397
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-04-18 15:26:43 -0300

    style: :lipstick: adiciona estilo para os comentários

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/emanuelalderete.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Karol Rodrigues</strong>
+                            <time
+                                title='Publicado em 23 de maio de 2024 às 14:04'
+                                dateTime='2025-01-01 13:34:35'>Cerca de 1h atrás</time>
+                        </div>
+                        <button title='Deletar comentário'>
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>Muito bom, Emanuel! Parabéns!</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+
+        </div>
+    );
+}
