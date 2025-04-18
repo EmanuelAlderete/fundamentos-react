32a8f9a18b52f93301eb29e1163bffafcf7973cb
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-04-18 16:30:07 -0300

    feat: implementa uso de props no post

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,51 @@ import './assets/global.css';
 import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/emanuelalderete.png",
+      name: "Emanuel Alderete",
+      role: "Web Developer"
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala Galera!' },
+      { type: 'paragraph', content: 'Esse é meu post!' },
+      { type: 'link', content: 'https://github.com/emanuelalderete' }
+    ],
+    publishedAt: new Date('2023-05-03 20:00:00')
+  },
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/emanuelalderete.png",
+      name: "Emanuel Alderete",
+      role: "Web Developer"
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala Galera!' },
+      { type: 'paragraph', content: 'Esse é meu post!' },
+      { type: 'link', content: 'https://github.com/emanuelalderete' }
+    ],
+    publishedAt: new Date('2023-05-03 20:00:00')
+  },
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/emanuelalderete.png",
+      name: "Emanuel Alderete",
+      role: "Web Developer"
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala Galera!' },
+      { type: 'paragraph', content: 'Esse é meu post!' },
+      { type: 'link', content: 'https://github.com/emanuelalderete' }
+    ],
+    publishedAt: new Date('2023-05-03 20:00:00')
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -11,22 +56,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Emanuel"
-            content="Meu primeiro post"
-          />
-          <Post
-            author="Emanuel"
-            content="Meu primeiro post"
-          />
-          <Post
-            author="Emanuel"
-            content="Meu primeiro post"
-          />
-          <Post
-            author="Emanuel"
-            content="Meu primeiro post"
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,37 +1,39 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { Comment } from './Comment';
 import { Avatar } from './Avatar';
 import styles from './Post.module.css';
 
-export function Post() {
+export function Post({ author, content, publishedAt }) {
+
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", { locale: ptBR });
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: "Há"
+
+    });
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://github.com/emanuelalderete.png" />
+                    <Avatar src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
-                        <strong>Emanuel Alderete</strong>
-                        <span>Web Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
-                <time title='Publicado em 23 de maio de 2024 às 14:04' dateTime='2025-01-01 13:34:35'>Publicado há 1h</time>
+                <time
+                    title={publishedDateFormatted}
+                    dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
             </header>
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-                <p>Tem novidade no meu portifa! Acabei de subir um
-                    projeto que desenvolvi com muito carinho e foco em
-                    resolver dores reais de quem trabalha na área contábil.
-                    O nome do projeto é ContadorPro 📊🧾</p>
-                <p>É um sistema feito pra facilitar a vida dos contadores:
-                    gestão de clientes, prazos, documentos e muito mais.
-                    Tudo de forma simples e eficiente!</p>
-                <p><a href="">seulink.com/contadorpro</a></p>
-                <p>
-                    <a href="">#novo</a>{' '}
-                    <a href="">#projeto</a>{' '}
-                    <a href="">#contabilidade</a>{' '}
-                    <a href="">#sistemaparaContadores</a>{' '}
-                    <a href="">#backend</a>
-                </p>
+                {content.map(line => {
+                    if (line.type === 'paragraph') {
+                        return <p>{line.content}</p>
+                    } else if (line.type === 'link') {
+                        return <p><a>{line.content}</a></p>
+                    }
+                })}
             </div>
             <form className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
