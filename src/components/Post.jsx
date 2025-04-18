dc486f7325df75697f9a0a96999a0d6dca7aa3f7
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-04-18 14:40:57 -0300

    style: :lipstick: ajusta estilo do Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -11,8 +11,37 @@ export function Post() {
                         <span>Web Developer</span>
                     </div>
                 </div>
-                <time>Publicado há 1h</time>
+                <time title='Publicado em 23 de maio de 2024 às 14:04' dateTime='2025-01-01 13:34:35'>Publicado há 1h</time>
             </header>
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Tem novidade no meu portifa! Acabei de subir um
+                    projeto que desenvolvi com muito carinho e foco em
+                    resolver dores reais de quem trabalha na área contábil.
+                    O nome do projeto é ContadorPro 📊🧾</p>
+                <p>É um sistema feito pra facilitar a vida dos contadores:
+                    gestão de clientes, prazos, documentos e muito mais.
+                    Tudo de forma simples e eficiente!</p>
+                <p><a href="">seulink.com/contadorpro</a></p>
+                <p>
+                    <a href="">#novo</a>{' '}
+                    <a href="">#projeto</a>{' '}
+                    <a href="">#contabilidade</a>{' '}
+                    <a href="">#sistemaparaContadores</a>{' '}
+                    <a href="">#backend</a>
+                </p>
+            </div>
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea
+                    placeholder='Deixe um comentário' />
+                <footer>
+                    <button type="submit">Comentar</button>
+                </footer>
+            </form>
+
+
         </article>
     )
 }
